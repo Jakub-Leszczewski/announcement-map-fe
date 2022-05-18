a83e431c05defee47841072a9cc5da5cbecdd033
JakubL <jakubxleszczewski@gmail.com>
2022-05-18 19:04:43 +0200

    create simple structure for login view and other user nav windows

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import { MainNav } from '../nav/MainNav'
+import { UserMenu } from '../userMenu/UserMenu'
 
 function App() {
   return (
     <div className="App">
       <MainNav/>
+      <UserMenu/>
     </div>
   );
 }
---
+++ src/view/loginChoice/LoginChoice.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+export const UserMenu = () => {
+  const isOpen = true;
+
+  if(!isOpen) return null;
+
+  return(
+    <section className="UserMenu__section">
+
+    </section>
+  );
+}
