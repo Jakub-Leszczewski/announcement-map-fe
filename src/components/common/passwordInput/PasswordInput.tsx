de585bc8423223aebbc58b86b1ad7f596dd96cc3
JakubL <jakubxleszczewski@gmail.com>
2022-05-21 21:18:37 +0200

    Connect form with state
    - create reducer for userForm
    - create validation for signup page
    - create a component that combines password and repeat password

--- src/components/common/passwordInput/PasswordInput.tsx
+++ src/components/common/passwordInput/PasswordInput.tsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import './style.css';
 
 interface Props {
   placeholder: string;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  onFocus?: (e: ChangeEvent<HTMLInputElement>) => void;
+  onBlur?: (e: ChangeEvent<HTMLInputElement>) => void;
+  value?: string;
 }
 
-export const PasswordInput = ({ placeholder }: Props) => {
+export const PasswordInput = ({ placeholder, value, onChange, onFocus, onBlur }: Props) => {
   const [visible, setVisible] = useState<boolean>(false);
 
   const onMouseDown = () => {
@@ -22,6 +26,13 @@ export const PasswordInput = ({ placeholder }: Props) => {
         className="PasswordInput__input"
         type={visible ? "text" : "password"}
         placeholder={placeholder}
+        required={true}
+        minLength={8}
+        maxLength={36}
+        onChange={onChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        value={value}
       />
       <div className="PasswordInput__icon" onMouseDown={onMouseDown} onMouseUp={onMouseUp}>
         {
--- src/components/common/shortTextInput/ShortTextInput.tsx
+++ src/components/common/shortTextInput/ShortTextInput.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react'
 import './style.css';
 
 interface Props {
   placeholder: string;
+  email?: boolean;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  value?: string;
 }
 
-export const ShortTextInput = ({placeholder}: Props) => {
+export function ShortTextInput({
+  placeholder, email, required, minLength, maxLength, onChange, value
+}: Props) {
   return (
     <input
       className="ShortTextInput"
-      type="text"
+      type={email ? 'email' : 'text'}
       placeholder={placeholder}
+      required={required}
+      minLength={minLength ?? 0}
+      maxLength={maxLength ?? 255}
+      onChange={onChange}
+      value={value}
     />
   );
 }
