4aa020434063700107fdcf0703e9dd024dc653cb
JakubL <jakubxleszczewski@gmail.com>
2022-06-01 01:34:47 +0200

    Changes styles for resposive

--- src/components/AdPopup/AdPopup.tsx
+++ src/components/AdPopup/AdPopup.tsx
@@ -3,13 +3,52 @@ import './AdPopup.css'
 import { AdInfo } from '../AdInfo/AdInfo'
 
 interface Props {
-
+  id: string;
+  name: string;
+  description: string
+  price: number;
+  country: string;
+  city: string;
+  zipCode: string;
+  address: string;
+  date: Date;
+  links: {
+    id: string;
+    name: string;
+    url: string;
+  }[];
 }
 
-export const AdPopup = ({}: Props) => {
+export const AdPopup = ({
+  id, name, description, price, country, city, zipCode, address, date, links
+}: Props) => {
   return (
     <section className="AdPopup">
-      <AdInfo/>
+      <AdInfo
+        id={id}
+        name={name}
+        description={description}
+        price={price}
+        country={country}
+        city={city}
+        zipCode={zipCode}
+        address={address}
+        date={date}
+        links={links}
+      />
+      <hr/>
+      <AdInfo
+        id={id}
+        name={name}
+        description={description}
+        price={price}
+        country={country}
+        city={city}
+        zipCode={zipCode}
+        address={address}
+        date={date}
+        links={links}
+      />
     </section>
   )
 }
