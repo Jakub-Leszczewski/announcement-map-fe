d9790da399030fd40cb697f080b39857fce7acd3
JakubL <jakubxleszczewski@gmail.com>
2022-06-06 20:00:25 +0200

    add categories from backend

--- src/components/MainNav/MainNav.tsx
+++ src/components/MainNav/MainNav.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { SearchBar } from '../common/SearchBar/SearchBar';
+import { SearchBar } from '../SearchBar/SearchBar';
 import { Button } from '../common/Button/Button';
-import { Select } from '../common/Select/Select';
+import { SelectCategories } from '../SelectCategories/SelectCategories';
 import './MainNav.css';
 import { User } from '../User/User'
-import { useApiAuth } from '../../hooks/useApiAuth'
 import { useDispatch } from 'react-redux'
-import { setJwt } from '../../store/slices/user-slice'
 import { openAddAnnouncement } from '../../store/slices/app-slice'
+import { useApi } from '../../hooks/useApi'
+import { CategoryEntity } from 'types';
 
 export function MainNav() {
+  const [loading, status, categories] = useApi<CategoryEntity[]>('http://localhost:3001/api/category');
   const dispatch = useDispatch();
 
   const goAddAnnouncement = () => {
@@ -20,15 +21,7 @@ export function MainNav() {
       <div className="MainNav__left-side">
         <SearchBar />
         <Button onClick={goAddAnnouncement}>Dodaj ogłoszenie</Button>
-        <Select
-          initialCategory={{name: 'wszystko', value: '*'}}
-          categories={[
-            { id: '1', name: 'Motoryzacja' },
-            { id: '2', name: 'Elektronika' },
-            { id: '3', name: 'Ogród' },
-            { id: '4', name: 'Zabawki' },
-          ]}
-        />
+        <SelectCategories firstOption={{ name: 'Wszystko', value: '*' }}/>
       </div>
 
       <div className="MainNav__right-side">
