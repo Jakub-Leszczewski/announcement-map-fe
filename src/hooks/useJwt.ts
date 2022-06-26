7068ea1b241e3170efc62cdcf3c518d2d17d13dd
JakubL <jakubxleszczewski@gmail.com>
2022-06-27 01:27:59 +0200

    fix problems with type in authContext

--- src/hooks/useJwt.ts
+++ src/hooks/useJwt.ts
@@ -4,7 +4,7 @@ import { AuthContext } from '../components/Auth/Auth'
 export const useJwt = (): string | null => {
   const authContext = useContext(AuthContext);
 
-  if(authContext && authContext.id && authContext.exp * 1000 > Date.now()) {
+  if(authContext && 'id' in authContext && authContext.exp * 1000 > Date.now()) {
     return authContext.jwt;
   }
 
